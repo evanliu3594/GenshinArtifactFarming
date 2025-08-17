b6dff6cb78f6be192ac2458d9286edd8453cbd10
yifan.liu <yifan.liu@smail.nju.edu.cn>
2025-08-18 02:06:04 +0800

    update readme

--- routeOpt_MIP.R
+++ routeOpt_MIP.R
@@ -49,65 +49,66 @@
   PointData %>% filter(数量 != 有摩拉 + 无摩拉)
 
   PointData$数量 %>% sum()
+}
 
-  getRouteData <- function(
-    pun_comf = 6,
-    pun_map = 6,
-    t_teleport = 6,
-    block_lvl = 10,
-    reuse12 = FALSE
-  ) {
-    Route_design <- read_xlsx(
-      '1.Data/路线设计.xlsx',
-      range = cell_cols("A:D")
+getRouteData <- function(
+  pun_comf = 6,
+  pun_map = 6,
+  t_teleport = 6,
+  block_lvl = 10,
+  reuse12 = FALSE
+) {
+  Route_design <- read_xlsx(
+    '1.Data/路线设计.xlsx',
+    range = cell_cols("A:D")
+  ) %>%
+    mutate(
+      路线 = hex_encode(路线),
+      rand = runif(n(), 0, .01),
+      time = `基础耗时(s)` + rand,
+      time_tp = time + t_teleport,
+      time_pun = time_tp +
+        replace_na(地图惩罚, 0) * pun_map +
+        replace_na(舒适度惩罚, 0) * pun_comf,
     ) %>%
-      mutate(
-        路线 = hex_encode(路线),
-        rand = runif(n(), 0, .01),
-        time = `基础耗时(s)` + rand,
-        time_tp = time + t_teleport,
-        time_pun = time_tp +
-          replace_na(地图惩罚, 0) * pun_map +
-          replace_na(舒适度惩罚, 0) * pun_comf,
-      ) %>%
-      select(路线, time, time_tp, time_pun)
+    select(路线, time, time_tp, time_pun)
 
-    Route_data <- Route_design %>%
-      mutate(points = 路线, 路线 = as_factor(路线)) %>%
-      separate_longer_delim(points, delim = "——") %>%
-      left_join(PointData, join_by(points == 点位)) %>%
-      filter(!is.na(主地图)) %>%
-      mutate(mora_drop = 有摩拉 * 200) %>%
-      group_by(路线) %>%
-      summarise(
-        time = mean(time, na.rm = T),
-        time_tp = mean(time_tp, na.rm = T),
-        time_pun = mean(time_pun, na.rm = T),
-        点数 = sum(数量, na.rm = T),
-        期望摩拉 = sum(mora_drop, na.rm = T),
-        期望狗粮 = sum(数量 * 掉落期望 * 420, na.rm = T),
-        封锁等级 = mean(封锁等级, na.rm = T),
-        allFast = all(刷新周期 != "24")
-      ) %>%
-      mutate(
-        路线 = as.character(路线),
-        狗粮效率 = 期望狗粮 / time_tp,
-        综合资源 = 期望摩拉 + 期望狗粮,
-        综合效率 = 综合资源 / time_tp
-      ) %>%
-      filter(封锁等级 < block_lvl) %>% #view()
-      select(-封锁等级)
+  Route_data <- Route_design %>%
+    mutate(points = 路线, 路线 = as_factor(路线)) %>%
+    separate_longer_delim(points, delim = "——") %>%
+    left_join(PointData, join_by(points == 点位)) %>%
+    filter(!is.na(主地图)) %>%
+    mutate(mora_drop = 有摩拉 * 200) %>%
+    group_by(路线) %>%
+    summarise(
+      time = mean(time, na.rm = T),
+      time_tp = mean(time_tp, na.rm = T),
+      time_pun = mean(time_pun, na.rm = T),
+      点数 = sum(数量, na.rm = T),
+      期望摩拉 = sum(mora_drop, na.rm = T),
+      期望狗粮 = sum(数量 * 掉落期望 * 420, na.rm = T),
+      封锁等级 = mean(封锁等级, na.rm = T),
+      allFast = all(刷新周期 != "24")
+    ) %>%
+    mutate(
+      路线 = as.character(路线),
+      狗粮效率 = 期望狗粮 / time_tp,
+      综合资源 = 期望摩拉 + 期望狗粮,
+      综合效率 = 综合资源 / time_tp
+    ) %>%
+    filter(封锁等级 < block_lvl) %>% #view()
+    select(-封锁等级)
 
-    Route_data_fast <- Route_data %>% filter(allFast) %>%
-      mutate(路线 = str_replace_all(路线, "(——)([^——]+)", "\\1\\2rep"))
+  Route_data_fast <- Route_data %>% filter(allFast) %>%
+    mutate(路线 = str_replace_all(路线, "(——)([^——]+)", "\\1\\2rep"))
 
-    if (reuse12) {
-      bind_rows(Route_data, Route_data_fast) %>% select(-allFast)
-    } else {
-      Route_data %>% select(-allFast)
-    }
+  if (reuse12) {
+    bind_rows(Route_data, Route_data_fast) %>% select(-allFast)
+  } else {
+    Route_data %>% select(-allFast)
   }
 }
+
 
 # Optim Core ====
 
